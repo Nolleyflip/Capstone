420337de5c0e27b69bdb3a25f1b105c85a2d052b
Nolleyflip <157773672+Nolleyflip@users.noreply.github.com>
2024-10-30 10:17:14 -0700

    second commit

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   database: 'auth',
   password: 'adnama12',
   port: 5432,
+  connection: 'postgresql://whatever:obiWmRunlRBCWCfb0OA4j9fdX3maA6pu@dpg-csh6ek08fa8c73f6thf0-a/capstone_database_d3rl'
 })
 
 module.exports = {
